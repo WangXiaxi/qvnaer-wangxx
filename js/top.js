5352d756670f5db5e100d56ceb188367f45d44c8
王小星 <995107408@qq.com>
2016-08-08 11:29:47 +0800

    scrollTop

--- js/top.js
+++ js/top.js
@@ -14,38 +14,37 @@ $.fn.extend({
 		});
 	},
 	offSet :  function(){
-		var NavChoSco = function(eq1,_this){
-				$(_this).find(".Nav-cho").find("a").eq(eq1).addClass("color").siblings().removeClass("color");
+		var NavChoSco = function(eq1){
+				$(".Nav").find(".Nav-cho").find("a").eq(eq1).addClass("color").siblings().removeClass("color");
 			};
-		$(document).scroll(function(){
-			var $scTop =  $(window).scrollTop();
+		$(window).scroll(function(){
+			var $scTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
 			if ($scTop >= 480){
 				$(".Nav").addClass("fixed");
 			}else if($scTop < 480){
 				$(".Nav").removeClass("fixed");
 			}
-			$(".oneMian").getCur(".oneMian",837,2043,1409);
-			$(".twoMian").getCur(".twoMian",2325,3515,2897);
-			$(".threeMian").getCur(".threeMian",3818,5305,4385);
-			$(".fourMian").getCur(".fourMian",5587,7365,6159);
-			$(".sixMian").getCursix(".sixMian",8501,11423,9359,10217);
+			$(".oneMian").getCur(".oneMian",837,2043,1409,$scTop);
+			$(".twoMian").getCur(".twoMian",2325,3515,2897,$scTop);
+			$(".threeMian").getCur(".threeMian",3818,5305,4385,$scTop);
+			$(".fourMian").getCur(".fourMian",5587,7365,6159,$scTop);
+			$(".sixMian").getCursix(".sixMian",8501,11423,9359,10217,$scTop);
 			if ($scTop>=641 && $scTop < 2129){
-				NavChoSco(1,this);
+				NavChoSco(1);
 			}else if ($scTop>=2129 && $scTop < 3617){
-				NavChoSco(2,this);
+				NavChoSco(2);
 			}else if($scTop>=3617 && $scTop < 5391){
-				NavChoSco(3,this);
+				NavChoSco(3);
 			}else if($scTop>=5391 && $scTop < 7451){
-				NavChoSco(4,this);
+				NavChoSco(4);
 			}else if($scTop>=7451 && $scTop < 8305){
-				NavChoSco(5,this);
+				NavChoSco(5);
 			}else if($scTop>=8305 ){
-				NavChoSco(6,this);
+				NavChoSco(6);
 			}
 		});	
 	},
-	getCur : function(sele,start,end,curstart){
-		var $scTop =  $(window).scrollTop();
+	getCur : function(sele,start,end,curstart,$scTop){
 		if($scTop >= start&&$scTop < end){
 			$(sele + " .free-go").addClass("fixed");
 			if($scTop < curstart){
@@ -65,8 +64,7 @@ $.fn.extend({
 			$(window).scrollTop(curstart);
 		});
 	},
-	getCursix : function(sele,start,end,curstart,cur2){
-		var $scTop =  $(window).scrollTop();
+	getCursix : function(sele,start,end,curstart,cur2,$scTop){
 		if($scTop >= start&&$scTop < end){
 			$(sele + " .free-go").addClass("fixed");
 			if($scTop < curstart){
@@ -91,7 +89,6 @@ $.fn.extend({
 	},
 	NaClick : function(){
 		var NavCho = [641,2129,3617,5391,7451,8305];
-		
 		$(this).find(".Nav-cho").find("a").click(function(){
 			var index = $(this).index();
 			$(window).scrollTop(NavCho[index]);
